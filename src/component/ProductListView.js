9d08e129c2ea7dd4afad04c7dbfecfd85ad02d24
amitdev053 <amitgarg0534@gmail.com>
2023-09-09 20:06:00 +0530

    Adding Product List View Component

--- src/component/ProductListView.js
+++ src/component/ProductListView.js
@@ -3,18 +3,15 @@ import Loader from "./Loader";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from "react-toastify";
 import ProductSlider from "./ProductSlider";
-import ProductListView from "./ProductListView";
 
 
-export default function Products() {
+export default function ProductListView() {
   const baseUrl = "https://fakestoreapi.com";
   const productsUrl = "/products";
   const cartUrl = "/carts";
   const [loader, setloader] = useState(true);
   const [allproduct, setAllProduct] = useState([]);
   const [SliderHeading, setSliderHeading] = useState("Choose Your Favourite");
-  const [productView, setProductView] = useState(true)
- 
 
 
   function getProducts(baseUrl, productsUrl) {
@@ -51,27 +48,6 @@ export default function Products() {
   useEffect(() => {
     getProducts(baseUrl, productsUrl);
   }, []);
-
- function  toggleView(){
- 
-
-  if(productView === true){
-    setProductView(false)
-    document.getElementById('listType').style.backgroundColor = "black"
-    document.getElementById('listType').style.color = "white"
-  }else{
-    setProductView(true)
-    document.getElementById('listType').style.backgroundColor = "#eee"
-    document.getElementById('listType').style.color = "black"
-
-
-  }
- 
-
-  
- }
-  
-
   if (loader === true) {
     return (
       <>
@@ -81,7 +57,7 @@ export default function Products() {
   } else {
     return (
       <>
-        <div className="container text-left mt-74">
+        <div className="container text-left mt-1">
         <ToastContainer
 position="top-right"
 autoClose={3000}
@@ -95,23 +71,7 @@ pauseOnHover
 theme="dark"
 
 />
-<div style={{display:"flex", justifyContent:"space-between", alignItems:"center", padding:"0px 10px 0px 0px"}}>
-      <div
-            style={{
-              fontSize: "25px",
-              fontWeight: "700",
-              marginBottom: "20px",
-            }}
-          >
-            Flash Products
-          </div>
-          <i className={`fa-solid fa-list-ul product_view_icon grid_view`} onClick={toggleView} id="listType"></i>
-          </div>
-         {
-          (productView === true) ?           
-          
-              <>
-              <div className="row">
+  <div className="row flex-column">
             {/* Columns Started Here */}
             {allproduct.map((product) => {
               const formatter = new Intl.NumberFormat("en-US", {
@@ -120,25 +80,27 @@ theme="dark"
               });
 
               return (
-                <div className="col-md-3 col-sm-12 gallerycol" key={product.id}>
-                  <div className="galleryimg position-relative">
+                <div className="col-12  gallerycol list_view_cols" key={product.id}>
+                  <div className="galleryimg position-relative list_view_product_image">
                     <img src={product.image} id="productimg" alt="" />
                     <span id="productprice" className="productprice">
                       {formatter.format(product.price)}
                     </span>
                   </div>
                   <div className="mediacontent d-inline-block">
-                    <p className="mb-1 totalgal">
+                    
+                    <h4 className="gallerytitle productname" id="productname">
+                      {product.title}
+                    </h4>
+                    
+                    <div className="gallerytitle productname productdiscripation" id="productname">
+                      {product.description}
+                    </div>
+                    <p className="mt-1 totalgal">
                       Rating: {product.rating.rate}
                     </p>
-                    <h4 className="gallerytitle productname" id="productname">
-                      {product.title.slice(0, 37)}...
-                    </h4>
-                    <div className="gallerytitle productname productdiscripation" id="productname">
-                      {product.description.slice(0, 100)}...
-                    </div>
                     
-                    <div class="p_btns mt-3">
+                    <div class="p_btns mt-3 list_view_action_button">
                       <button
                         className="btn btn-sm btn-primary p_s_btn  mr-1 brand_button"
                         onClick={() => {
@@ -166,14 +128,7 @@ theme="dark"
             })}
             {/* Columns End Upon the div Here */}
           </div>
-          </>
-          
-          :
-          <ProductListView />
-         
-         }
         </div>
-<ProductSlider heading={SliderHeading}/>
 
 
       </>

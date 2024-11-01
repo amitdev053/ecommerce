06b82632da117da0dc83e53779b174d26849a894
amitdev053 <amitgarg0534@gmail.com>
2024-11-01 20:46:50 +0530

    adding share functionality in userLike Products

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -379,6 +379,36 @@ function RouteToLikeProducts(){
   navigate("/products")
 }
 
+const handleShare = async (productTitle, productDesc, productImage) => {
+  console.log("Attempting to share content:", productTitle, productDesc, productImage);
+
+  if (navigator.canShare && navigator.canShare({ files: [new File([""], "test.jpg", { type: "image/jpeg" })] })) {
+    try {
+      const response = await fetch(productImage);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const blob = await response.blob();
+      console.log("Image fetched successfully, creating file...");
+
+      const file = new File([blob], 'image.jpg', { type: 'image/jpeg' });
+      console.log("File created successfully:", file);
+console.log("Attempting to share content:",file, [file], URL.createObjectURL(file))
+      await navigator.share({
+        title: productTitle,
+        text: productDesc,
+        url: window.location.href,
+        files: [file],
+      });
+
+      console.log('Content shared successfully');
+    } catch (error) {
+      console.error('Error sharing content:', error);
+    }
+  } else {
+    alert('Web Share API is not supported in your browser or the current device cannot share files.');
+  }
+};
   
   if (loader === true) {
     return (
@@ -690,7 +720,7 @@ function RouteToLikeProducts(){
                        Cart <i className="fa-solid fa-cart-plus icon_margin"></i>
                        
                       </button>
-                      <button className="btn btn-sm btn-primary p_s_btn brand_button ">
+                      <button className="btn btn-sm btn-primary p_s_btn brand_button " onClick={()=>{handleShare(likeItems.title, likeItems.description,  likeItems.image )}}>
                         Share <i class="fa-solid fa-share-nodes icon_margin"></i>
                       </button>
                     </div>

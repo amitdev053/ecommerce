fd3e544534251c45b9a253101deba97fdc176a4d
amitdev053 <amitgarg0534@gmail.com>
2025-01-01 16:36:00 +0530

    adding times in explore tab

--- src/component/Explore.js
+++ src/component/Explore.js
@@ -5,14 +5,19 @@ import axios from "axios";
 import EmptyCartImage from "../appimages/empty_cart.webp";
 
 const Explore = () => {
-  let content = [ "Travel", "Fashion", "Sports", "Music", "Gaming", "Technology", "Health", "Finance", "Education", "Lifestyle"]
-  let index = 0;
+  // let content = [ "couples", "Fashion", "Sports", "Music", "Gaming", "Technology", "Health", "Finance", "Education", "Lifestyle"]
+  let content = [
+    "kisses", "Fashion", "Sports", "Music", "Gaming", "Technology", "Health", "Finance", "Education", "Lifestyle", "Travel and Adventure",    "Art and Creativity", "Nature and Wildlife", "Food and Culinary", "History and Culture","Fitness and Wellness", "Architecture and Design","Space and Astronomy", "Books and Literature",
+    "Motivation and Productivity",  "Luxury and Lifestyle",  "Science and Innovation",   "Minimalism and Aesthetics", "Photography and Videography",  "Pets and Animals",
+    "DIY and Crafting",  "Sustainability and Eco-living", "Hobbies and Skills",  "Vintage and Retro", "couples"
+  ];
+  
 
-const baseUrl = `https://pixabay.com/api/?key=45283300-eddb6d21a3d3d06f2a2381d7d&q=${content[index]}&image_type=photo`
-
-
-const [loader, setloader] = useState(true);
-const [images, setImages] = useState([]);
+  
+  const [index, setIndex] = useState(0);
+  const [loader, setloader] = useState(true);
+  const [images, setImages] = useState([]);
+  const baseUrl = `https://pixabay.com/api/?key=45283300-eddb6d21a3d3d06f2a2381d7d&q=${content[index]}&image_type=photo`
 
 
   function getProducts(url) {
@@ -28,16 +33,26 @@ const [images, setImages] = useState([]);
         setImages(result.hits);
       });
   }
-  setInterval(() => {
-    index = (index + 1) % content.length;
-    const newBaseUrl = `https://pixabay.com/api/?key=45283300-eddb6d21a3d3d06f2a2381d7d&q=${content[index]}&image_type=photo`
-    getProducts(newBaseUrl)
-    // Update the baseUrl variable or make an API call with the new URL
-  }, 7200000); // 2 hours in milliseconds
+
   useEffect(() => {
-    document.title = "Explore";
+    document.title = `Explore Images for ${content[index].toUpperCase()}`;
+
+    // Fetch images for the initial index
     getProducts(baseUrl);
+
+    // Update index and fetch images every 2 hours
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % content.length);
+    }, 1 * 60 * 60  * 1000); // 1 hours in milliseconds
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(interval);
   }, []);
+
+  // Fetch images whenever the index changes
+  useEffect(() => {
+    getProducts(baseUrl);
+  }, [index]);
 
   if (loader === true) {
     return (
